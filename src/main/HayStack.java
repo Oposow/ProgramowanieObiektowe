0df0639f417cd8c01fc3977c1034ddc8adf6d66b
student18 <student18@pcoip.ki.agh.edu.pl>
2016-11-10 18:27:47 +0100

    init

---
+++ src/main/HayStack.java
@@ -0,0 +1,22 @@
+package main;
+
+public class HayStack {
+
+	public Position pos; 
+	
+	public HayStack(Position _pos)
+	{
+		pos = _pos;
+	}
+	
+	public Position getPosition()
+	{
+		return pos;
+	}
+	
+	public String toString()
+	{
+		return "s";
+	}
+	
+}
---
+++ src/main/UnboundedMap.java
@@ -0,0 +1,35 @@
+package main;
+
+public class UnboundedMap implements IWorldMap {
+
+	@Override
+	public boolean canMoveTo(Position position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Car car) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void run(MoveDirection[] directions) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isOccupied(Position position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object objectAt(Position position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
